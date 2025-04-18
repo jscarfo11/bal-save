ccf6ad2913798fec7f1504bacd1bebba9238f072
Jack Scarfo <jackdscarfo@gmail.com>
2025-04-18 14:37:05 -0400

    Add support for a logo when I get around to getting someone to make one

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use bal_save::MyApp;
 
 fn main() -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions::default();
+    
+
+    let mut native_options = eframe::NativeOptions::default();
+    let viewport = egui::ViewportBuilder::default().with_title("bal_save").with_icon(eframe::icon_data::from_png_bytes(&include_bytes!("assets/temp_logo.png")[..])
+    .unwrap());
+
+    native_options.viewport = viewport;
     eframe::run_native(
-        "browse",
+        "bal_save",
         native_options,
         Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
     )
